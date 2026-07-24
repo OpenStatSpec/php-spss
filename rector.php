b9ee6a5310b4d9e5290ce29c7c53eeaab8d8753c
Tõnis Ormisson <tonis@andmemasin.eu>
2026-07-24 18:10:59 +0300

    Keep automated formatting checks idempotent

--- rector.php
+++ rector.php
@@ -3,12 +3,16 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\CodingStyle\Rector\Stmt\NewlineAfterStatementRector;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
         __DIR__ . '/examples',
+    ])
+    ->withSkip([
+        NewlineAfterStatementRector::class,
     ])
     ->withPhpSets()
     ->withPreparedSets(
